76168e7fb83c29357fe7cb5b481416ea0738a4ba
suzuki071 <shinee.0711.st@gmail.com>
2021-08-18 16:01:02 +0000

    [Fix]ユーザページレイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,33 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "編集しました"
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
   end
-  
+
   def following
     @user = User.find(params[:id])
     @users = @user.following
     render 'show'
   end
-  
+
   def followers
     @user = User.find(params[:id])
     @users = @user.followers
     render 'show'
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
