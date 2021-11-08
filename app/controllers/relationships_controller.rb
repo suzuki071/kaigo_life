3e03ee9125a62da4dc3af9c960ad7aecf01a1154
suzuki071 <shinee.0711.st@gmail.com>
2021-11-08 08:28:56 +0000

    フォロー機能修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,22 @@
 class RelationshipsController < ApplicationController
 
   def create
-    @user = User.find(params[:following_id])
-    current_user.follow(@user)
-    
     following = current_user.relationships.build(follower_id: params[:user_id])
     following.save
-    redirect_to request.referrer || root_path
+
+    @user = User.find(params[:following_id])
+    current_user.follow(@user)
+    # redirect_to request.referrer || root_path
   end
 
   def destroy
-    @user = User.find(params[:id])
-    current_user.unfollow(@user)
-    
     following = current_user.relationships.find_by(follower_id: params[:user_id])
     following.destroy
-    redirect_to request.referrer || root_path
+
+    @user = User.find(params[:id])
+    current_user.unfollow(@user)
+
+    # redirect_to request.referrer || root_path
   end
 
 end
